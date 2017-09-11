43edf08693e85a8e558901deba7ba8f7976da60a
Menka <jag2780@hotmail.com>
2017-09-11 20:05:16 +1000

    Sprouts-demo

---
+++ demo_leds/leds_code.ino
@@ -0,0 +1,51 @@
+#include <Zumo32U4.h>
+
+
+Zumo32U4LCD lcd;
+
+void setup() {
+  // put your setup code here, to run once:
+  //clear the led screen
+  lcd.clear();
+
+  //put "LED Example" on the display
+  //Line 0 
+  lcd.gotoXY(0,0);
+  lcd.print(" Hi!");
+
+  //Line 1
+  lcd.gotoXY(0,1);
+  lcd.print("Menka");
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  //Turn on the red Led and turn off the others
+  //1 means turn on
+  //0 means turn off
+  ledRed(1);
+  ledYellow(0);
+  ledGreen(0);
+
+  //wait for 1 second = 1000 ms
+  delay(1000);
+
+  //turn on the yellow led, and turn off the others
+  ledRed(0);
+  ledYellow(1);
+  ledGreen(0);
+
+  //wait for 1 second
+  delay(1000);
+
+  
+  //turn on the yellow led, and turn off the others
+  ledRed(0);
+  ledYellow(0);
+  ledGreen(1);
+
+    //wait for 1 second
+  delay(1000);
+}
---
+++ demo_move_turn/demo_move_turn.ino
@@ -0,0 +1,41 @@
+//import zumo libraries
+#include <Zumo32U4.h>
+
+//declare variables
+Zumo32U4LCD lcd;
+Zumo32U4Motors motors;
+
+//Question: where is the lcd screen?
+void setup() {
+  //clear the lcd screen
+  lcd.clear();
+  //print hello on line 0
+  lcd.gotoXY(0,0);
+  lcd.print("Hello!");
+  //print Menka on line 1
+  lcd.gotoXY(0,1);
+  lcd.print("Menka");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//150 rotations per minute
+  //++ move forward
+  //--move backward
+  //-+turns left
+  //+-turns right
+motors.setSpeeds(200,200);
+delay(5000);
+motors.setSpeeds(-60,60);
+delay(7000);
+
+  // 1 means ON, 0 means OFF
+  //turn all leds ON
+  ledRed(1);
+  ledYellow(1);
+  ledGreen(1);
+  //wait for 1 second
+  delay(1000);
+  
+ 
+}
